139494f6933128a6347d5af1b8010121b762a259
David Tauraso <david318@me.com>
2020-02-23 19:55:53 -0800

    Put in migrations and started crud code

--- migrations/20200222124021_elementary_school_math_help.js
+++ migrations/20200222124021_elementary_school_math_help.js
@@ -1,10 +1,74 @@
 
 exports.up = function(knex) {
-  
+  // https://stackoverflow.com/questions/20619211/difference-between-cascade-and-restrict-sql-ddl-database/42774793
     return knex.schema
-        .createTable("users")
+        .createTable('users', tbl => {
+            tbl.increments();
+            tbl
+                .string('username', 128)
+                .notNullable()
+                .unique();
+            tbl.string('password', 128).notNullable();
+
+
+        })
+        .createTable('records', tbl => {
+            tbl.increments();
+            // day string
+            tbl.timestamp('createdAt').defaultTo(knex.fn.now());
+            tbl
+                .integer('howManyCorrect')
+                .unsigned()
+                .notNullable();
+            tbl
+                .integer('howManyAttempted')
+                .unsigned()
+                .notNullable();
+            
+        })
+        .createTable('problemsTried', tbl => {
+            tbl.increments();
+            tbl
+            .integer('recordId')
+            .unsigned()
+            .references('id')
+            .inTable('records')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+            .defaultTo(null);
+            tbl
+                .string('expression', 255)
+                .notNullable();
+            tbl
+                .boolean('isRight');
+            tbl
+                .enu('kindOfOperation', [
+                'Add',
+                'Subtract',
+              ]);
+
+        })
+
+
+    
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+        .dropTableIfExists('problemsTried')
+        .dropTableIfExists('records')
+        .dropTableIfExists('users');
 };
+
+// table for records 1 entry per day
+// id
+// day string
+// how many correct int
+// how many attempted int
+
+// table for problems tried
+// id
+// record_id maps to daily records id
+// expression string
+// wasRight boolean
+// kind of operation (add, subtract, decimal values) enum
