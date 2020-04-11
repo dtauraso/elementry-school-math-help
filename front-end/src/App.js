926552cfff3141fa40ff1c2ea38c2947184aeafa
David Tauraso <david318@me.com>
2020-04-10 20:48:35 -0700

    Added nav bar and route support

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom'
+import styled from 'styled-components'
 import logo from './logo.svg';
 import './App.css';
 // import { AddTwoValues } from './Components/AddTwoValues'
 import PresentProblems from './Components/PresentProblems'
+
+
 function App() {
   return (
     <div className="App">
-      <PresentProblems />
+      <nav className="NavBar">
+        <Link to="/problemSet">
+          Problems
+        </Link>
+        <Link to='/results'>
+          Results
+        </Link>
+      </nav>
+      <Route path="/problemSet"
+        render={(props) => <PresentProblems />}
+        />
+      {/* <Route path="/results"
+        render={(props) => } */}
+      
       {/* <AddTwoValues a={4} b={3}/> */}
     </div>
   );
