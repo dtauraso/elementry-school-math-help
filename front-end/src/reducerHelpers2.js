df014d1eef0c29d7330a63e61c5650f301259816
David Tauraso <david318@me.com>
2021-03-26 13:56:46 -0700

    Updated code

--- front-end/src/reducerHelpers2.js
+++ front-end/src/reducerHelpers2.js
@@ -6,8 +6,45 @@ export const getVariable2 = () => {
 
 }
 
-export const set2 = () => {
+export const set2 = (parentDataKey, referenceToParentObject, varName, newValue) => {
+    // the react components will travel down the state chart
+    // when loading components
 
+    /*
+    parentKey iteraion count
+    
+    stateName could match parentDataState1 or not
+    stateName: {
+        Set2StateFunctionCallCount: 0
+        timeLine: [
+            {
+                parentDataStateAbsolutePath: {
+                    before: {
+                        var1: 
+                        var2:
+                    }
+                    after: {
+                        var1:
+                        var2
+                    }
+                }
+            },
+            {
+                parentDataState2: {
+                    before: {
+                        var1: 
+                        var2:
+                    }
+                    after: {
+                        var1:
+                        var2
+                    }
+                }
+            }
+        ] 
+    }
+    
+    */
 }
 
 export const treeVisualizer2 = () => {
@@ -15,5 +52,5 @@ export const treeVisualizer2 = () => {
 }
 
 export const breathFirstTraversal2 = () => {
-    
+
 }
