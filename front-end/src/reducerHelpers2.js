0f5d83c6a395ab6f14a1b5b71642c18d52949e88
David Tauraso <david318@me.com>
2021-03-26 13:14:46 -0700

    Updated code

---
+++ front-end/src/reducerHelpers2.js
@@ -0,0 +1,19 @@
+
+export const setVariable2 = () => {
+
+}
+export const getVariable2 = () => {
+
+}
+
+export const set2 = () => {
+
+}
+
+export const treeVisualizer2 = () => {
+
+}
+
+export const breathFirstTraversal2 = () => {
+    
+}
